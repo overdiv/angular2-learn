d5911422732a8f67f78d47ec4ff2cf4010c7463d
overdiv <stancode203@gmail.com>
2020-02-07 00:03:05 +0900

    쿼리파라미터 값 가져오기

--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
 
 @Component({
   selector: 'app-login',
@@ -6,10 +7,20 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./login.component.scss']
 })
 export class LoginComponent implements OnInit {
+  url;
 
-  constructor() { }
+  constructor(private route: ActivatedRoute) { }
 
   ngOnInit() {
+    // 비동기 방식
+    this.route.queryParamMap.subscribe((data => {
+      console.log(data.get('url'));
+      this.url = data.get('url');
+    }));
+
+    // 동기방식
+    // console.log(this.route.snapshot.queryParamMap.get('url'));
+    // this.url = this.route.snapshot.queryParamMap.get('url');
   }
 
 }
